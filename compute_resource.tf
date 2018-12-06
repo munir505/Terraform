cb7d9c68e7efb4082a44e4aea4d56e4ce55744f2
munir505 <31385912+munir505@users.noreply.github.com>
2018-12-06 15:46:29 +0000

    Update compute_resource.tf

--- compute_resource.tf
+++ compute_resource.tf
@@ -1,40 +1,41 @@
 resource "google_compute_instance" "default" {
-	name = "${var.name}"
-	machine_type = "${var.machine_type}"
-	zone = "${var.zone}"
-	tags = "${var.tags}"
-	boot_disk {
-		initialize_params {
-			image = "centos-7"
-		}
-	}
-	network_interface {
-		network = "default"
-		access_config {
-			// Ephermal IP
-		}
-	}
-	metadata {
-		sshKeys = "terraform:${file("~/.ssh/id_rsa.pub")}"
-	}
-	connection = {
-		type = "ssh"
-		user = "terraform"
-		private_key = "${file("~/.ssh/id_rsa")}"
-	}
-	provisioner "remote-exec" {
-		inline = [
-			"echo Hello World"
-		]
-	}
-	provisioner "remote-exec" {
-		scripts = [
-			"${var.scripts}"
-		]
-	}
+        name = "${var.name}"
+        machine_type = "${var.machine_type}"
+        zone = "${var.zone}"
+        tags = "${var.tags}"
+        boot_disk {
+                initialize_params {
+                        image = "centos-7"
+                }
+        }
+        network_interface {
+                network = "default"
+                access_config {
+                        // Ephermal IP
+                }
+        }
+        metadata {
+                sshKeys = "terraform:${file("~/.ssh/id_rsa.pub")}"
+        }
+        connection = {
+                type = "ssh"
+                user = "terraform"
+                private_key = "${file("~/.ssh/id_rsa")}"
+        }
+        provisioner "remote-exec" {
+                inline = [
+                        "echo Hello World"
+                ]
+        }
+        provisioner "remote-exec" {
+                scripts = [
+                        "${var.scripts}"
+                ]
+        }
 }
 
 resource "google_compute_attached_disk" "default" {
-	disk = "jenkins-jobs"
-  	instance = "go-server"
+        zone = "${var.zone}"
+        disk = "jenkins-jobs"
+        instance = "${google_compute_instance.default.self_link}"
 }
